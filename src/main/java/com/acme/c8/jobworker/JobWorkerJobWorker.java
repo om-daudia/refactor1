f377d1cfc92784d0e90729661908d8ceb2fc2e70
Yatin Singla <180975158+yatin-singla-pb@users.noreply.github.com>
2026-02-19 18:18:21 +0530

    fix attempt 1

--- src/main/java/com/acme/c8/jobworker/JobWorkerJobWorker.java
+++ src/main/java/com/acme/c8/jobworker/JobWorkerJobWorker.java
@@ -1,15 +1,16 @@
 package com.acme.c8.jobworker;
 
 import com.acme.c8.jobworker.util.DmnEvaluator;
-import io.camunda.zeebe.client.api.response.ActivatedJob;
-import io.camunda.zeebe.spring.client.annotation.JobWorker;
-import io.camunda.zeebe.spring.client.annotation.Variable;
-import io.camunda.zeebe.spring.common.exception.ZeebeBpmnError;
+import io.camunda.client.annotation.JobWorker;
+import io.camunda.client.annotation.Variable;
+import io.camunda.client.api.response.ActivatedJob;
+import io.camunda.client.api.worker.JobClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -19,40 +20,54 @@ public class JobWorkerJobWorker {
     private final JobWorkerJobWorkerService service;
 
     @JobWorker(type = "com.capbpm.c8.JobWorker.FindUser:v.1.1", fetchVariables = {"userId"})
-    public Map<String, Object> findUser(final ActivatedJob job, @Variable  String userId) {
-    final String  METHOD_NAME= "JobWorker.findUser";
-    Map<String,Object> inputVarMap = job.getVariablesAsMap();
-        log.trace(METHOD_NAME+" started...");
+    public Map<String, Object> findUser(JobClient client, final ActivatedJob job, @Variable String userId) {
+        final String METHOD_NAME = "JobWorker.findUser";
+        Map<String, Object> inputVarMap = job.getVariablesAsMap();
+        log.trace(METHOD_NAME + " started...");
 
         try {
             Map<String, Object> outputs = service.findUserImpl(userId);
 
-            log.trace(METHOD_NAME+" Finished.");
+            log.trace(METHOD_NAME + " Finished.");
             return outputs;
         } catch (Exception e) {
-            log.trace(METHOD_NAME+" Error.");
-            throw new ZeebeBpmnError("ERR_CODE", e.getMessage(),inputVarMap);
+            log.trace(METHOD_NAME + " Error.");
+            client.newThrowErrorCommand(job.getKey())
+                    .errorCode("ERR_CODE")
+                    .errorMessage(e.getMessage())
+                    .variables(inputVarMap)
+                    .send()
+                    .join();
+
+            return Map.of();
         }
     }
 
     @JobWorker(type = "com.capbpm.c8.JobWorker.filterPatients:v.1.1", fetchVariables = {"index"})
-    public Map<String, Object>  sift(final ActivatedJob job, @Variable  Integer index) {
-        final String  METHOD_NAME= "JobWorker.findUser";
-        Map<String,Object> inputVarMap = job.getVariablesAsMap();
-        log.trace(METHOD_NAME+" started...");
+    public Map<String, Object> sift(JobClient client, final ActivatedJob job, @Variable Integer index) {
+        final String METHOD_NAME = "JobWorker.findUser";
+        Map<String, Object> inputVarMap = job.getVariablesAsMap();
+        log.trace(METHOD_NAME + " started...");
 
         try {
-           long duration= DmnEvaluator.go(index);
+            long duration = DmnEvaluator.go(index);
 
-           Map<String, Object> outputs = new HashMap<>();
-           outputs.put("duration", duration);
-           System.out.println("duration="+duration);
+            Map<String, Object> outputs = new HashMap<>();
+            outputs.put("duration", duration);
+            System.out.println("duration=" + duration);
 
-           // log.trace(METHOD_NAME+" Finished.");
+            // log.trace(METHOD_NAME+" Finished.");
             return outputs;//n outputs;
         } catch (Exception e) {
-            log.trace(METHOD_NAME+" Error.");
-            throw new ZeebeBpmnError("ERR_CODE", e.getMessage(),inputVarMap);
+            log.trace(METHOD_NAME + " Error.");
+            client.newThrowErrorCommand(job.getKey())
+                    .errorCode("ERR_CODE")
+                    .errorMessage(e.getMessage())
+                    .variables(inputVarMap)
+                    .send()
+                    .join();
+
+            return Map.of();
         }
     }
 }
